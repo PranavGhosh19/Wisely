59eea8c5cd2ad099172e644f96eb0efd007036d4
Pranav Ghosh <pranavghosh19@gmail.com>
2026-04-22 08:31:45 +0000

    the landing page, make it light theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Onest:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
-        <meta name="theme-color" content="#07161B" />
+        <meta name="theme-color" content="#F7F8FA" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
@@ -58,7 +58,7 @@ export default function RootLayout({
       <body className="font-body antialiased bg-background text-foreground transition-colors duration-300">
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
